4ebf0e632a481eb19de55d0f9298d137c0f761ed
Johanna <johannarklay@gmail.com>
2018-09-29 16:45:17 -0500

    handle not selecting a pokemon

--- pokedex/src/components/DetailView.js
+++ pokedex/src/components/DetailView.js
@@ -4,15 +4,29 @@ import './styles/DetailView.css';
 const DetailView = ({ pokemon }) => {
   const { id, name, sprite, type } = pokemon;
 
+  if(id != null)
+  {
+    return (
+        <section className="detail-view">
+        <h1 className="data-tile">1st Gen Pokedex</h1>
+        <img src={sprite} className='sprite-image' alt="sprite"/>
+        <div className='data-wrapper'>
+            <h1 className='data-name'>ID: {id} {name}</h1>
+            <p className="data-char">Type: {type}</p>
+        </div>
+        </section>
+    )
+    }
+    else
+    {
   return (
-    <section className="detail-view">
-      <img src={sprite} className='sprite-image' alt="sprite"/>
-      <div className='data-wrapper'>
-        <h1 className='data-name'>ID: {id} {name}</h1>
-        <p className="data-char">Type: {type}</p>
-      </div>
-    </section>
-  )
+        <section className="detail-view">
+        <h1 className="data-tile">1st Gen Pokedex</h1>
+        <div className='sprite-image'> Choose a pokemon!
+        </div>
+        </section>
+    )
+    }
 }
 
 export default DetailView;
