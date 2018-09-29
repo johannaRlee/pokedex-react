e1f07702cbc3ced77fc6c9f555a299a8c50fe5a1
Johanna <johannarklay@gmail.com>
2018-09-29 16:56:48 -0500

    add height and weight to detail

--- pokedex/src/components/DetailView.js
+++ pokedex/src/components/DetailView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles/DetailView.css';
 
 const DetailView = ({ pokemon }) => {
-  const { id, name, sprite, type } = pokemon;
+  const { id, name, sprite, type, weight, height } = pokemon;
 
   if(id != null)
   {
@@ -11,8 +11,10 @@ const DetailView = ({ pokemon }) => {
         <h1 className="data-tile">1st Gen Pokedex</h1>
         <img src={sprite} className='sprite-image' alt="sprite"/>
         <div className='data-wrapper'>
-            <h1 className='data-name'>ID: {id} {name}</h1>
+            <h3 className='data-name'>ID: {id} {name}</h3>
             <p className="data-char">Type: {type}</p>
+            <p className="data-char">Weight: {weight} kg</p>
+            <p className="data-char">Height: {height} m</p>
         </div>
         </section>
     )
